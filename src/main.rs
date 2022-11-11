d3230b9164e7a38b8372ca45af506fac094de5e4
Brandon Barker <brandon.barker@gmail.com>
2022-11-11 14:59:33 -0500

    starting to add entities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bracket_lib::prelude::{FontCharType, GameState, /* Rltk,*/ VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World,
+}
+
 fn main() {
+    let mut gs = State { ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    // Note we aren't storing the entity, just telling the World it is there.
+    gs.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: bracket_lib::prelude::to_cp437('@'),
+            fg: RGB::named(bracket_lib::prelude::YELLOW),
+            bg: RGB::named(bracket_lib::prelude::BLACK),
+        })
+        .build();
+
     println!("Hello, world!");
 }
