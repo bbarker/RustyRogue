35f492a04469b3531a0eb3083265b68fdce459a2
Brandon Barker <brandon.barker@gmail.com>
2022-11-13 12:47:09 -0500

    added player movement

--- src/main.rs
+++ src/main.rs
@@ -34,15 +34,20 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        player_input(self, ctx);
+
         self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
-
         (&positions, &renderables).join().for_each(|(pos, render)| {
             ctx.set(pos.xx, pos.yy, render.fg, render.bg, render.glyph);
-        });
+        })
     }
 }
+
+#[derive(Component, Debug)]
+struct Player {}
 
 struct LeftWalker {}
 
@@ -70,16 +75,17 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     // Note we aren't storing the entity, just telling the World it is there.
     // FIXME: unit discard warning?
-    build_entity_at(&mut gs);
+    build_entity_player(&mut gs);
     build_entities_happy_folk(&mut gs);
 
     bracket_lib::prelude::main_loop(context, gs).unwrap()
 }
 
-fn build_entity_at(gs: &mut State) -> Entity {
+fn build_entity_player(gs: &mut State) -> Entity {
     gs.ecs
         .create_entity()
         .with(Position { xx: 40, yy: 25 })
@@ -88,6 +94,7 @@ fn build_entity_at(gs: &mut State) -> Entity {
             fg: RGB::named(bracket_lib::prelude::YELLOW),
             bg: RGB::named(bracket_lib::prelude::BLACK),
         })
+        .with(Player {})
         .build()
 }
 
@@ -106,4 +113,29 @@ fn build_entities_happy_folk(gs: &mut State) -> Vec<Entity> {
                 .build()
         })
         .collect()
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    (&mut players, &mut positions)
+        .join()
+        .for_each(|(_player, pos)| {
+            pos.xx = min(79, max(0, pos.xx + delta_x));
+            pos.yy = min(49, max(0, pos.yy + delta_y));
+        })
+}
+
+fn player_input(gs: &mut State, ctx: &mut BTerm) {
+    match ctx.key {
+        None => {}
+        Some(key) => match key {
+            // Player Movement
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
 }
